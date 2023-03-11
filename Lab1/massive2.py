b4e46d824e901a26a3705501fbce90ba4ea98e36
Koros-AD <118260412+Koros-AD@users.noreply.github.com>
2023-03-11 18:53:46 +0300

    Update massive2.py

--- Lab1/massive2.py
+++ Lab1/massive2.py
@@ -1,14 +1,20 @@
 from collections import Counter
+import random
+a = []
 
-n = int(input("n= "))
-a = [int(i) for i in input("a= ").split()]
-counter = Counter(a)
-result = a[0]
-max_count = counter[result]
+n=(int(input("n= ")))
+for i in range (n):
+     x=random.randint(1,100)
+     a.append(x)
 
-for number, count in counter.items():
-    if count > max_count or (count == max_count and number < result):
-        result = number
-        max_count = count
-
-print(result)
+def most_common_element(lst):
+    counter = Counter(lst)
+    most_common = counter.most_common()
+    max_count = most_common[0][1]
+    result = most_common[0][0]
+    for elem, count in most_common:
+        if count == max_count and elem > result:
+            result = elem
+    return result
+print(a)
+print(most_common_element(a))
