bee1dfc15c191f47d844e5747b6d1255d3a9b1ac
Koros-AD <118260412+Koros-AD@users.noreply.github.com>
2023-03-27 01:04:10 +0300

    Update table_iter.py

--- Lab1/table_iter.py
+++ Lab1/table_iter.py
@@ -1,3 +1,5 @@
+#Itertools code that calculates how many codewords containing only 5 letters (А,Т,О,М) could be created, M can be used only once,
+#others can be repeated or not used at all.
 import itertools
 k = 0
 a = list(itertools.product("атом", repeat = 6))
