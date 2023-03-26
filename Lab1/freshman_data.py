d42c0aceb83f9501b22b947e913788fa2c9ade1b
Koros-AD <118260412+Koros-AD@users.noreply.github.com>
2023-03-27 00:56:16 +0300

    Update freshman_data.py

--- Lab1/freshman_data.py
+++ Lab1/freshman_data.py
@@ -1,10 +1,13 @@
+#The program reads csv file and calculates the weight difference from September to April. According to the problem conditions, only male respondants 
+# with BMI in April greater than 20 and non-negative weight difference are output.
 import csv
 from datetime import datetime
-
+#reading the file
 with open('D:\Media/freshman_kgs.csv', 'r') as f:
     reader = csv.DictReader(f)
     results = []
-
+    
+#calculating weight difference from Sep to Apr for male respondents
     for column in reader:
 
         if column['Sex'] == 'M':
@@ -13,12 +16,14 @@ with open('D:\Media/freshman_kgs.csv', 'r') as f:
             apr_weight = float(column["Weight (Apr)"])
 
             weight_diff = apr_weight - sep_weight
-
+            
+#setting condition for BMI and weight difference. If wd is >0 and BMI is 20> respondent is added to "results" list
             bmi_apr = float(column['BMI (Apr)'])
             if weight_diff >= 0 and bmi_apr > 20:
                 column['Weight diff'] = weight_diff
                 results.append(column)
-
+                
+#creating a file with new data, creating new column for Weight Difference
     with open('D:\Media/freshman_kgs_new.csv', 'w', newline='') as f:
         fieldnames = reader.fieldnames + ['Weight diff']
         writer = csv.DictWriter(f, fieldnames=fieldnames)
