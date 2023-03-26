9e59e73e3b80d0096284fae3ee7bb2887e31570b
Koros-AD <118260412+Koros-AD@users.noreply.github.com>
2023-03-27 00:46:01 +0300

    Update bookcount.py

--- Lab1/bookcount.py
+++ Lab1/bookcount.py
@@ -1,12 +1,15 @@
+#The program reads json file and outputs books with over 500 pages
 import json
-
+#opening file
 with open('D:\Media/books.json', 'r') as f:
     books = json.load(f)
-
+    
+#creating a list "result" for books that fit the criteria
+# setting the condition: if pageCount is over 500 add a book to the list
 result = []
 for book in books:
     if book['pageCount'] > 500:
         result.append(book)
-
+#output
 for book in result:
     print(book)
