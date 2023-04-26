bf2d94643c69fd9457868842d1015cf600d24756
Koros-AD <118260412+Koros-AD@users.noreply.github.com>
2023-04-26 10:04:25 +0300

    Update Lab2.4.py

--- Lab2/Lab2.4.py
+++ Lab2/Lab2.4.py
@@ -1,4 +1,4 @@
-#4)  Используя pandas, напишите скрипт, который загружает CSV-файл в DataFrame и отображает первые 5 строк df.
+#4)   Используя pandas, напишите скрипт, который загружает CSV-файл в DataFrame и отображает первые 5 строк df.
 import pandas as pd
 df=pd.read_csv("D:/Media/email.csv")
 print(df.head())
