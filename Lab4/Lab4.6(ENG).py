fa0a541ea7d664ee31095142904b28537210feea
Koros-AD <118260412+Koros-AD@users.noreply.github.com>
2023-05-08 05:40:47 +0300

    Add files via upload

---
+++ Lab4/Lab4.6(ENG).py
@@ -0,0 +1,11 @@
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+
+text = "Barack Obama was the 44th president of the United States of America. He was born in Honoluly, Hawaii in 1961"
+
+tokens = nltk.word_tokenize(text)
+pos_tags = nltk.pos_tag(tokens)
+chunks = nltk.ne_chunk(pos_tags)
+
+print(chunks)
---
+++ Lab4/Lab4.6(RUS).py
@@ -0,0 +1,11 @@
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+
+text = "Санкт-Петербург - город на северо-западе Российской Федерации, расположенный на реке Нева."
+
+tokens = word_tokenize(text)
+pos_tags = nltk.pos_tag(tokens, lang='rus')
+chunks = nltk.ne_chunk(pos_tags, binary=False)
+
+print(chunks)
