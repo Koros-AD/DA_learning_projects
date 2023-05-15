9ef4501aaf44f33981f8aeb869e4095c991dac00
Koros-AD <118260412+Koros-AD@users.noreply.github.com>
2023-05-15 03:53:28 +0300

    Add files via upload

---
+++ Lab5/Lab5.1.py
@@ -0,0 +1,9 @@
+import cv2
+img=cv2.imread('D:\Media/leaf.jpg')
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+ret, thresh = cv2.threshold(gray, 100, 255, cv2.THRESH_BINARY)
+
+cv2.imshow("Original", img)
+cv2.imshow("Thresholded", thresh)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
